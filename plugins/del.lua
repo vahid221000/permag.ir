7cd128f90b5bc170bae67d7fb5958e03ebad07e8
vahid221000 <vahidupvc6@gmail.com>
2017-03-15 23:18:57 +0330

    vahid

--- plugins/del.lua
+++ plugins/del.lua
@@ -8,7 +8,7 @@ local function run(msg, matches)
     if msg.chat_id_:match("^-100") then
        if is_owner(msg) or is_mod(msg) then
           if tonumber(matches[2]) > 100 or tonumber(matches[2]) < 1 then
-             pm = '_ 100> ÊÚÏÇÏ íÇã åÇí ŞÇÈá ÍĞİ åÑ ÏİÚå >1 _'
+             pm = '_ 100> ÃŠÃšÃÃ‡Ã ÂÃ­Ã‡Ã£ Ã¥Ã‡Ã­ ÃÃ‡ÃˆÃ¡ ÃÃÃ Ã¥Ã‘ ÃÃÃšÃ¥ >1 _'
              tdcli.sendMessage(msg.chat_id_, data.msg.id_, 1, pm, 1, 'html')
              else
           tdcli_function ({
@@ -18,11 +18,11 @@ local function run(msg, matches)
     offset_ = 0,
     limit_ = tonumber(matches[2])
   }, delmsg, nil)
-             pm ='*'..matches[2]..'* _íÇã ÇÎíÑ Ç˜ ÔÏ_'
+             pm ='*'..matches[2]..'* _ÂÃ­Ã‡Ã£ Ã‡ÃÃ­Ã‘ ÂÃ‡Ëœ Ã”Ã_'
              tdcli.sendMessage(msg.chat_id_, msg.id_, 1, pm, 1, 'html')
          end
      end
- else pm ='Çíä Çã˜Çä İŞØ ÏÑ _ÓæÑ Ñæå_ ãã˜ä ÇÓÊ.'
+ else pm ='Ã‡Ã­Ã¤ Ã‡Ã£ËœÃ‡Ã¤ ÃÃÃ˜ ÃÃ‘ _Ã“Ã¦ÂÃ‘ ÂÃ‘Ã¦Ã¥_ Ã£Ã£ËœÃ¤ Ã‡Ã“ÃŠ.'
     tdcli.sendMessage(msg.chat_id_, msg.id_, 1, pm, 1, 'html')
 end
 end
@@ -34,6 +34,5 @@ return {
     },
     run = run
 }
--- http://permag.ir
--- @permag_ir
--- @permag_bots
+
+-- @sendpmmbot
